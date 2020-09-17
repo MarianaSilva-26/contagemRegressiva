92c264a8cd4d24936a01b9fdee26beb67b53055d
marianaSilva-26 <mariana.silvadossantos.26@gmail.com>
2020-09-17 16:46:22 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace contagemRegressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("preparar...!");
+            Console.ReadLine();
+            Console.WriteLine("apontar...!");
+            Console.ReadLine();
+            Console.WriteLine("fogo...!");
+            Console.ReadLine();
+        }
+    }
+}
